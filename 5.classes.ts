ff95f0493af26e3164d6bccaf773d5ea699727d1
gurvsxen487 <gauravsaxena.cs@gmail.com>
2018-12-15 17:28:38 +0530

    classes,methods and constructors

---
+++ 5.classes.ts
@@ -0,0 +1,22 @@
+class Person {
+    firstName:string;
+    lastName:string;
+
+    // can't overload constructor
+    constructor( firstName:string,lastName:string ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // methods
+    getfullname = () => this.firstName+ ' ' +this.lastName;
+}
+
+// var aPerson:Person = new Person();
+var aPerson = new Person('Gaurav','Saxena');
+
+// aPerson.firstName='Gaurav';
+// aPerson.lastName='Saxena';
+// console.log(aPerson);
+
+console.log(aPerson.getfullname());
