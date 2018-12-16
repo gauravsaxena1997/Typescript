4f7c9102f0604b1e4d0c83a9e06cde97cfc39aef
gurvsxen487 <gauravsaxena.cs@gmail.com>
2018-12-16 23:07:09 +0530

    type definations

--- npm_project/index.ts
+++ npm_project/index.ts
@@ -1,7 +1,12 @@
 import {Person} from './person';
+import * as _ from 'lodash';
 
 let aPerson  = new Person();
 
 aPerson.firstName='Gaurav';
 aPerson.lastName="Saxena";
 aPerson.getfullname();
+
+let myArr:number[];
+myArr=[1,2,3,4,5,6,7,8,9];
+console.log(_.reverse(myArr));
--- npm_project/out/index.js
+++ npm_project/out/index.js
@@ -1,7 +1,18 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var person_1 = require("./person");
+var _ = __importStar(require("lodash"));
 var aPerson = new person_1.Person();
 aPerson.firstName = 'Gaurav';
 aPerson.lastName = "Saxena";
 aPerson.getfullname();
+var myArr;
+myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(_.reverse(myArr));
