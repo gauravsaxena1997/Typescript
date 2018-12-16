005ccf2822f22327741129ee691257dcb0b52767
gurvsxen487 <gauravsaxena.cs@gmail.com>
2018-12-16 14:00:12 +0530

    duck typing

--- 7.interface.ts
+++ 7.interface.ts
@@ -11,3 +11,18 @@ class Foo implements Person{
         return this.firstName+' '+this.lastName;
     }
 }
+
+let aPerson:Person   = new Foo();
+
+let someObj = {
+    firstName:'string',
+    lastName:'string',
+    other:10,
+    getfullName:()=>'string'
+};
+
+// Duck Typing (structure based typing) : when property matches
+aPerson = someObj;
+
+// cannot access the properties whichare not present in the interface
+// aPerson.other
