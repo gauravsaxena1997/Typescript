5646da84fd2a7dd053c9c810758767abd4f58635
gurvsxen487 <gauravsaxena.cs@gmail.com>
2018-12-17 11:46:04 +0530

    command line argument

--- github_report/out/index.js
+++ github_report/out/index.js
@@ -1,10 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var GithubApiService_1 = require("./GithubApiService");
+var lodash_1 = __importDefault(require("lodash"));
 var svc = new GithubApiService_1.GithubApi();
-svc.getUserInfo('gurvsxen487', function (user) {
-    svc.getRepos('gurvsxen487', function (repos) {
-        user.repos = repos;
-        console.log(user);
+if (process.argv.length < 3)
+    console.log('Please pass a username as command line argument.');
+else {
+    var userName_1 = process.argv[2];
+    svc.getUserInfo(userName_1, function (user) {
+        svc.getRepos(userName_1, function (repos) {
+            var sorted_array = lodash_1.default.sortBy(repos, [function (repo) { return repo.size * -1; }]);
+            var filtered_array = lodash_1.default.take(sorted_array, 5);
+            user.repos = filtered_array;
+            console.log(user);
+        });
     });
-});
+}
--- github_report/src/index.ts
+++ github_report/src/index.ts
@@ -1,12 +1,21 @@
 import {GithubApi} from './GithubApiService';
+import _ from 'lodash';
 import { User } from './User';
 import { Repo } from './Repo';
 
 let svc = new GithubApi();
-svc.getUserInfo('gurvsxen487', (user:User)=>{
-    svc.getRepos('gurvsxen487', (repos:Repo[])=>{
-        user.repos = repos;
-        console.log(user);
+if (process.argv.length<3)  
+    console.log('Please pass a username as command line argument.')
+else {
+    let userName = process.argv[2];
+    svc.getUserInfo(userName, (user:User)=>{
+        svc.getRepos(userName, (repos:Repo[])=>{
+            let sorted_array = _.sortBy(repos, [(repo:Repo) => repo.size * -1]);
+            let filtered_array = _.take(sorted_array,5);
+            user.repos = filtered_array;
+            console.log(user);
+        });
     });
-});
+}
+
 
